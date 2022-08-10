964fa59a06eb08dfd2d5b8eeab620e99e2bf2407
ahmetal <aalkis1135@gmail.com>
2022-08-10 20:55:41 +0100

    second commit from ahmet

--- src/Ahmet.java
+++ src/Ahmet.java
@@ -1,5 +1,7 @@
 public class Ahmet {
     public static void main(String[] args) {
         System.out.println("first change from ahmet");
+        System.out.println("first change from ahmet");
+
     }
 }
